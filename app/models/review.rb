820aba69399f8965a9f794198137f255752e8543
Blanca Rivero <briveroanglada@gmail.com>
2021-11-10 15:59:33 +0000

    all validations are done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :content, presence: true, allow_blank: false
 end
